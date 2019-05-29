10d069eb44c412ee64621b5bf72eb31e7c81df9b
deyrupam <rupamdey161@gmail.com>
2019-05-29 19:35:06 +0530

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use App\User;
+Use Auth;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+
+    }
+    public function handleProviderCallback()
+    {
+        // $user = Socialite::driver('google')->stateless()->user();
+        // return $user->name;
+         try {
+
+            $user = Socialite::driver('google')->stateless()->user();
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser){
+
+                Auth::login($finduser);
+
+                return redirect('/home');
+
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->back();
+            }
+
+        } catch (Exception $e) {
+            return redirect('login/google');
+        }
+
+    }
+
+}
